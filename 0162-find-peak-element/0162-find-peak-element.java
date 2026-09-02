7e30ff194b231eff5311ddc397cc85cb8ce0fdcf
Anshif <ansh988820@gmail.com>
2026-09-02 15:41:56 +0530

    Time: 0 ms (100%), Space: 44.5 MB (5.84%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+         int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // If the middle element is smaller than its right neighbor,
+            // we are on an upward slope. The peak must be on the right side.
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } 
+            // Otherwise, we are on a downward slope. 
+            // The middle element itself could be the peak, or it's on the left side.
+            else {
+                right = mid;
+            }
+        }
+        
+        // 'left' and 'right' converge to the index of a peak element
+        return left;
+        
+    }
+}
